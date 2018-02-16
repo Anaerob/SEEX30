2de08b5e00410f90d48e693e805cdd822e0c6009
Anaerob <linus.nor@gmail.com>
2018-02-16 14:52:39 +0100

    put 0-values for all 165 moves

--- main.py
+++ main.py
@@ -10,14 +10,14 @@ battle.trainers[1].setTeam(c.t1)
 
 battle.printSelf()
 
-for i in range(0, 6):
-  battle.blue.nextMove = 1
-  battle.red.nextMove = 0
+for i in range(0, 7):
+  battle.blue.nextMove = 2
+  battle.red.nextMove = 1
   battle.progress()
 
 while battle.running:
-  battle.blue.nextMove = 0
-  battle.red.nextMove = 0
+  battle.blue.nextMove = 1
+  battle.red.nextMove = 1
   battle.progress()
   # battle.printSelf()
 
