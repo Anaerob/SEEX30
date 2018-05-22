4458b5e63654870492e8de6142b2abdddd0fcbef
Anaerob <linus.nor@gmail.com>
2018-05-22 10:26:44 +0200

    add the other plots

---
+++ SSS/MCTS_plotSweepDS_time.py
@@ -0,0 +1,28 @@
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import numpy as np
+
+import Load
+
+def main():
+    
+    dSweep = ['5', '10', '15', '20', '25']
+    sSweep = ['200', '400', '600', '800', '1000']
+    results = Load.loadFloatMatrix('MCTS_sweepDS_time', len(dSweep), len(sSweep))
+    
+    plt.figure()
+    plot = plt.imshow(results, cmap = mpl.cm.Greys, norm = mpl.colors.Normalize(vmin = 0, vmax = 15), origin = 'lower')
+    cbar = plt.colorbar(plot)
+    cbar.ax.set_ylabel('Time (s)')
+    plt.title('Time consumed per game')
+    plt.xlabel('Search')
+    plt.xticks(list(range(len(sSweep))), sSweep)
+    plt.ylabel('Depth')
+    plt.yticks(list(range(len(dSweep))), dSweep)
+    
+    plt.show()
+
+if __name__ == '__main__':
+    main()
+
+#
