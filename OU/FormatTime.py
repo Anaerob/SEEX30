129205bd37aadab2df75295a10eca8239964f260
Anaerob <linus.nor@gmail.com>
2018-05-24 12:16:36 +0200

    renamed and reworked time printer

---
+++ OU/FormatTime.py
@@ -0,0 +1,57 @@
+
+def getString(time):
+    
+    if time < 1:
+        s = str(int(1000 * time)) + 'ms'
+    elif time < 60:
+        s = (str(int(time)) + 's, '
+            + str(int(1000 * (time % 1))) + 'ms')
+    elif time < 3600:
+        s = (str(int(time / 60)) + 'm, '
+            + str(int(time % 60)) + 's')
+    elif time < 86400:
+        s = (str(int(time / 3600)) + 'h, '
+            + str(int((time % 3600) / 60)) + 'm, '
+            + str(int(time % 60)) + 's')
+    else:
+        s = (str(int(time / 86400)) + 'd, '
+            + str(int((time % 86400) / 3600)) + 'h, '
+            + str(int((time % 3600) / 60)) + 'm')
+    return s
+
+def getMilliSeconds(time):
+    
+    if time < 1 / 1000:
+        return 0
+    else:
+        return int(1000 * (time))
+
+def getSeconds(time):
+    
+    if time < 1:
+        return 0
+    else:
+        return int(time)
+
+def getMinutes(time):
+    
+    if time < 60:
+        return 0
+    else:
+        return int(time / 60)
+
+def getHours(time):
+    
+    if time < 3600:
+        return 0
+    else:
+        return int(time / 3600)
+
+def getDays(time):
+    
+    if time < 86400:
+        return 0
+    else:
+        return int(time / 86400)
+
+#
